f2010577818cb068cfa79328dc13ac7a6698582f
sreekanth666 <srekthk@gmail.com>
2023-12-18 14:28:31 +0530

    Edit

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -6,7 +6,7 @@ function Dashboard() {
   const [selectedView, setSelectedView] = useState("manage")
   return (
     <div style={{height:'100dvh'}}>
-      <div className="row m-0 p-0" style={{minHeight:'100%'}}>
+      <div className="row m-0 p-0">
         <div className="col-sm-12 col-md-12 col-lg-2 col-xl-2 py-4" style={{backgroundColor:'#d4f3f3'}}>
           <button className='btn btn-light rounded-4 w-100 my-2 text-light' style={{backgroundColor:'#25aab7',fontWeight:'500'}} onClick={() => setSelectedView("manage")}>Manage Categories</button>
           <button className='btn btn-light rounded-4 w-100 my-2 text-light' style={{backgroundColor:'#25aab7',fontWeight:'500'}} onClick={() => setSelectedView("view")}>View List</button>
